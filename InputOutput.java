f9ba77149ca8ba94dcd6acb79da976bbce960a91
mfperdana13 <mfperdana13@gmail.com>
2020-04-05 17:12:13 +0800

    Buat File InputOutput

--- InputOutput.java
+++ InputOutput.java
@@ -1,11 +1,5 @@
-import java.util.Scanner;
-
 public class InputOutput{
- public static void main(string [] args) {
-  Scanner scanner = new Scanner(System.in);
-  String nama;
-  System.out.print("Masukan nama anda: ");
-  nama = scanner.nextLine();
+ public static void main(String [] args){
 
  }
-} 
+}
